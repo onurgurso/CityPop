e522ef8c78b90b73108d2c44c8e5c7a13e23c447
OGurso <70133220+OGurso@users.noreply.github.com>
2021-10-23 20:23:32 +0200

    Base file structure setup

---
+++ screens/ListScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ListScreen() {
+  return (
+    <View>
+      <Text>List Screen</Text>
+    </View>
+  );
+}
