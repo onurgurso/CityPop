150abc162d7217bb6bcc2afdaa21d2a1531695af
OGurso <70133220+OGurso@users.noreply.github.com>
2021-10-29 13:07:51 +0200

    Setup ListScreen to function properly

--- screens/ListScreen.tsx
+++ screens/ListScreen.tsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 import { StackNavigationProp } from "react-navigation-stack/lib/typescript/src/vendor/types";
+import { sortByPopulation } from "./globalFunctions";
+import List from "./../components/List";
+import { elements } from "./../styles/elements";
 
 interface Props {
   navigation: StackNavigationProp;
 }
 
 export default function ListScreen({ navigation }: Props) {
-  const searchResult = navigation.getParam("searchResult");
+  const [responseData, setResponseData] = useState({});
+
+  const selectedItem = navigation.getParam("SelectedItem");
+  const activeType = { screen: "Result" };
+
+  useEffect(() => {
+    fetch(
+      `http://api.geonames.org/searchJSON?username=weknowit&country=${selectedItem.countryCode}&cities1500&maxRows=10&orderby=population&featureCode=PPLA&featureCode=PPLC&&featureCode=PPL`
+    )
+      .then((data) => data.json())
+      .then((data) => {
+        setResponseData(sortByPopulation(data.geonames));
+      });
+  }, []);
 
   return (
-    <View>
-      <Text>{searchResult}</Text>
-      <Text>List item</Text>
-      <Text>List item</Text>
-      <Text onPress={() => navigation.navigate("Result")}>List Screen</Text>
+    <View style={elements.container}>
+      <Text style={elements.header}>{selectedItem.name}</Text>
+      <List
+        data={responseData}
+        activeType={activeType}
+        navigation={navigation}
+      />
     </View>
   );
 }
